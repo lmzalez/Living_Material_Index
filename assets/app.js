8e75a1e5e220deba1b76c5687dd449a5b82ac19d
Laura Maria Gonzalez <70725048+lmzalez@users.noreply.github.com>
2025-10-16 23:20:08 -0400

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,9 @@
+// highlight current nav link
+(function () {
+  const here = location.pathname.split('/').pop(); // e.g., "protocols.html" or ""
+  document.querySelectorAll('nav a').forEach(a => {
+    const target = a.getAttribute('href').split('/').pop();
+    if ((here === '' && target === './') || here === target) a.classList.add('active');
+  });
+})();
 
